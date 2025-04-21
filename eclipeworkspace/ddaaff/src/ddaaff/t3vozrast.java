7b04d1b562127f6036f43f07e43b82cc61bf7545
Bobux-many <oev2288@bk.ru>
2025-04-21 21:49:29 +0500

    Add files via upload

---
+++ eclipeworkspace/ddaaff/src/ddaaff/t3vozrast.java
@@ -0,0 +1,17 @@
+package ddaaff;
+
+public class t3vozrast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age = 18;
+		System.out.print("Жуковский ");
+		
+		System.out.println("18");
+		System.out.println("Жуковский Д.А. " + age);
+		
+		
+	}
+
+}
